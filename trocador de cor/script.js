994ea2026a23846b4fe082a95c0f8af81f1f6c0f
Alex Ponciano <73028034+alexponciano2@users.noreply.github.com>
2023-04-26 15:47:07 -0300

    Created color switch with DOM

---
+++ trocador de cor/script.js
@@ -0,0 +1,12 @@
+function clicar1(){
+    const DOM = document.querySelector('input.botao')
+    document.body.style.background = 'rgb(255, 38, 38)'
+}
+function clicar2(){
+    const DOM = document.querySelector('input.botao')
+    document.body.style.background = 'rgb(63, 255, 42)'
+}
+function clicar3(){
+    const DOM = document.querySelector('input.botao')
+    document.body.style.background = 'yellow'
+}
